7564e88f03d6e703aad32904264ff1beb386c97a
Swapnali-Patil-123 <pswapnali912@gmail.com>
2020-06-23 14:13:02 -0700

    first commit

---
+++ Hellworld.java
@@ -0,0 +1,19 @@
+public class Helloworld
+{
+        public static void main(String args[])
+        {
+                while(true)
+                {
+
+                        System.out.println("welcome to Terminal commands.");
+                        try{
+                                Thread.sleep(6000);
+
+                            }
+                        catch(Exception ex){}
+                }
+        }
+}
+
+
+
